7405ceee49f9a3cdf88113d88bade6a54c90e727
gokruzz <johnsonodukoyatemitope@gmail.com>
2023-10-05 19:56:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
-  *main - main entry point
-  *return : always 0 (succes)
-  */
+ * main - no argument
+ *
+ * Description: 'C program'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *return :always 0 (success)
-  */
+ * main - Entry point
+ *
+ * Description: 'C program'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
