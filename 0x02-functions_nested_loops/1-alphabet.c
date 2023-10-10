08421d47f09b6a3328a6460fd1b0ba164aa4308e
gokruzz <johnsonodukoyatemitope@gmail.com>
2023-10-10 09:04:07 -0700

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - check the code
+  *
+  * Return: Always 0.
+  */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
