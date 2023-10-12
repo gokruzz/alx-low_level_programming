8fe5b233565b41d881c4029dfcd31133fd83fb35
gokruzz <johnsonodukoyatemitope@gmail.com>
2023-10-12 15:52:29 -0700

    .

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_numbers - print 0 to 9
+  * Description: parameter c
+  * Return: void
+  */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c >= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
