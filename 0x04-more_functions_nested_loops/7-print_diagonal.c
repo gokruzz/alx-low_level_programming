b1a6da1247fb53db5585b48d25d162a4dea9b9fb
gokruzz <johnsonodukoyatemitope@gmail.com>
2023-10-13 09:26:20 -0700

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,21 +6,19 @@
   */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i;
-
 		for (i = 1; i <= n; i++)
 		{
-			int j;
-
 			for (j = 1; j <= i; j++)
 			{
-				_putchar(' ');
+				_putchar(32);
 			}
 			_putchar(92);
 			_putchar('\n');
