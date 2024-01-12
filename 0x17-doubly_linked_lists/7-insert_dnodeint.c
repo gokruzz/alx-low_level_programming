094f2996749d6c4f3959e6ec6a0884fa77055575
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-01-11 17:26:18 -0800

    error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,7 +5,6 @@
  * @h: pointer to a pointer
  * @idx: index of the list where new node will be added
  * @n:the element to be added
- *
  * Return: address of new node || NULL on failure
  */
 
@@ -14,8 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *tmp = *h;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -23,27 +20,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
+	if ((*h) == NULL)
+	{
+		if (idx == 0)
+		{
+			*h = new_node;
+			return (new_node);
+		}
+		return (NULL);
+	}
 	if (idx == 0)
 	{
-		if (*h != NULL)
-			(*h)->prev = new_node;
 		new_node->next = *h;
+		(*h)->prev = new_node;
 		*h = new_node;
+		return (new_node);
 	}
-
-	while (i < idx - 1)
+	while (tmp->next != NULL && i != idx)
 	{
-		if (tmp == NULL)
-			return (NULL);
 		tmp = tmp->next;
 		i++;
 	}
-	if (tmp == NULL)
-		return (NULL);
-	new_node->next = tmp->next;
-	new_node->prev = tmp;
-	if (tmp->next == new_node)
-		tmp->next->prev = new_node;
-	tmp->next = new_node;
-	return (new_node);
+	if (i == idx)
+	{
+		new_node->prev = tmp;
+		new_node->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = new_node;
+		tmp->next = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
