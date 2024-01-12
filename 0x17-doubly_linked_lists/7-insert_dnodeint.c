0ba217f0a7b2b35c828c83c547720918751c986d
gokruzz <johnsonodukoyatemitope@gmail.com>
2024-01-11 17:49:56 -0800

    fix.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: pointer to a pointer
@@ -10,37 +9,39 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *tmp;
+	dlistint_t *new_node, *tmp = *h;
 	unsigned int i = 0;
 
-	new_node = NULL;
-	if (idx == 0)
-		new_node = add_dnodeint(h, n);
-	else
-	{
-		tmp = *h;
-		i = 1;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-		if (tmp != NULL)
-			while (tmp->prev != NULL)
-				tmp = tmp->prev;
-		while (tmp != NULL)
+	if (*h == NULL)
+		if (idx == 0)
 		{
-			if (i == idx)
-				if (tmp->next == NULL)
-					new_node = add_dnodeint_end(h, n);
-				else
-					new_node = malloc(sizeof(dlistint_t));
-					if (new_node != NULL)
-						new_node->n = n;
-						new_node->next = tmp->next;
-						new_node->prev = tmp;
-						tmp->next->prev = new_node;
-						tmp->next = new_node;
-				break;
-			tmp = tmp->next;
-			i++;
+			*h = new_node;
+			return (new_node);
 		}
+		return (NULL);
+	if (idx == 0)
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	while (tmp->next != NULL && i != idx)
+	{
+		tmp = tmp->next;
+		i++;
 	}
-	return (new_node);
+	if (i == idx)
+		new_node->prev = tmp;
+		new_node->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = new_node;
+		tmp->next = new_node;
+		return (new_node);
+	return (NULL);
 }
